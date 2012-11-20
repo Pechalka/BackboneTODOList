9807a65e3feeb1dfa3868f6fb69a52b3cc3d5632
andrew <andrew@andrew-desktop.(none)>
2012-11-20 03:21:45 +0300

    refactoring

--- site/app.js
+++ site/app.js
@@ -1,21 +1,46 @@
-var templateHtml = "<a href='#'' id='showAdd'>add</a><div id='addContainer'></div><% _.each(obj, function(i) { %>  <p><%= i.title %></p> <a href='#' class='delete' data-id='<%= i._id %>'>delete</a> <a href='#' class='edit' data-id='<%= i._id %>'>edit</a><% }); %>";
-var eventHtml = "<input type='text' id='title' value='<%= title %>'/><a href='#' class='add'>save</a>";
+var templateHtml = "<a href='#' id='showAdd'>add</a><div id='addContainer'></div><div id='items'></div>";
+var eventHtml = "<input type='text' id='title' value='<%= title %>'/><a href='#' class='add'>save</a> <a href='#' class='cansel'>cansel</a>";
+var eventItemHtml = "<p><%= title %></p> <a href='#' class='delete'>delete</a> <a href='#' class='edit' >edit</a>";
 
 var EventView = Backbone.View.extend({
 	tpl: _.template(eventHtml),
 	events : {
-		'click .add' : 'save'
+		'click .add' : 'save',
+		'click .cansel' : 'cansel'
 	},
 	render : function() {
 		var html = this.tpl(this.model.toJSON());
 		this.$el.html(html);
 		return this;
 	},
+	cansel : function(){
+		this.remove();
+	},
 	save : function() {
 		var event = this.model.toJSON();
 		event.title = this.$('#title').val();
 		this.model.save(event);
-		this.$('#title').val('');
+		this.remove();
+	}
+});
+
+var EventItemView = Backbone.View.extend({
+	tpl: _.template(eventItemHtml),
+	events : {
+		'click .delete' : 'deleteEvent',
+		'click .edit' : 'editEvent'
+	},
+	deleteEvent : function(e) {
+		this.model.destroy();
+		this.remove();
+	},
+	editEvent : function(e) {
+		this.model.trigger('tryEdit', this.model);
+	},
+	render : function(){
+		var html = this.tpl(this.model.toJSON());
+		this.$el.html(html);
+		return this;
 	}
 });
 
@@ -23,48 +48,46 @@ var EventsView = Backbone.View.extend({
 	tpl: _.template(templateHtml),
 	parent : '.container',
 	events : {
-		'click #showAdd' : 'addEvent',
-		'click .delete' : 'deleteEvent',
-		'click .edit' : 'editEvent'
+		'click #showAdd' : 'addEvent'
 	},
 	initialize : function() {
-		this.model.on('all', this.render, this);
+		this.render();
+		this.model.on('all', this.renderItem, this);
 	},
 	render : function() {
 		var html = this.tpl(this.model.toJSON());
 		this.$el.html(html);
-		$(this.parent).empty().append(this.$el);
-		this.delegateEvents();
 
+		$(this.parent).empty().append(this.$el);
+	
 		return this;
 	},
-	deleteEvent : function(e) {
-		var id = $(e.target).data('id');
-		var event = this.model.get(id);
-	//	this.model.remove(event);///why??:))
-		event.destroy();
+	renderItem : function(){
+		console.log('render items');
+		this.$('#items').empty();
+		_.each(this.model.models, function(event){
+			var view = new EventItemView({ model : event });
+			event.on('tryEdit', this.prepareToSave, this);
+
+			this.$('#items').append(view.render().el)
+		}, this);
 	},
-	editEvent : function(e) {
-		var id = $(e.target).data('id');
-		var event = this.model.get(id);
-		this.changeOrCreateEvent(event);
-	},
-	changeOrCreateEvent : function(event) {
+	prepareToSave : function(event) {
 		var self = this;
 		event.on('change', function(m) {
 			self.model.add(m);
 		});
 		var view = new EventView({ model : event });
-		this.$('#addContainer').empty().append(view.render().el);
+		this.$('#addContainer').empty().append(view.render().$el);
 	},
 	addEvent : function() {
 		var event = new Event();
-		this.changeOrCreateEvent(event);
+		this.prepareToSave(event);
 	}
 });
 var Event = Backbone.Model.extend({
 	url : '/api/events',
-	 idAttribute: "_id",
+	idAttribute: "_id",
 	defaults : {
 		title : ''
 	}
@@ -72,19 +95,12 @@ var Event = Backbone.Model.extend({
 
 var EventCollection = Backbone.Collection.extend({
 	url : '/api/events',
-	model : Event,
-	initialize : function() {
-		this.eventsView = new EventsView({ model : this });
-	},
-	show : function() {
-		this.fetch();
-	}
+	model : Event
 });
 
 
 $(function() {
-	//$('#test').click(function() {
-		var events = new EventCollection;
-		events.show();
-	//});
+	var events = new EventCollection;
+	var eventsView = new EventsView({ model : events });
+	events.fetch();
 });
